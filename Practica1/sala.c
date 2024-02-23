a2a53b44e04bc53ef2e73bc795fd9fc78dbd8d6c
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-02-23 22:26:11 +0000

    Update sala.c
    Arreglos menores:
    Números de espaciados.

--- Practica1/sala.c
+++ Practica1/sala.c
@@ -7,12 +7,11 @@ int asientos_libres_variable;
 int asientos_ocupados_variable;
 
 
-
 int reserva_asiento(int id_persona){
 	if (asientos_libres_variable == 0){
 		return -1;
 	}
-	for (int count = 1; count<CAPACIDAD_MAXIMA; count++){
+	for (int count = 1; count<=CAPACIDAD_MAXIMA; count++){
 		if (*(asientos + count) == -1){
 			asientos_ocupados_variable++;
 			return *(asientos + count);
@@ -22,8 +21,8 @@ int reserva_asiento(int id_persona){
 
 
 int libera_asiento(int id_asiento){
-  *(asientos + id_asiento) = -1;
-  asientos_ocupados_variable--;
+	*(asientos + id_asiento) = -1;
+    asientos_ocupados_variable--;
 }
 
 
@@ -38,38 +37,38 @@ int estado_asiento(int id_asiento){
 
 
 int asientos_libres(){
-  return asientos_libres_variable;
+	return asientos_libres_variable;
 }
 
 
 int asientos_ocupados(){
-  return asientos_ocupados_variable;
+	return asientos_ocupados_variable;
 } 
 
 
 int capacidad_sala(){
-  return CAPACIDAD_MAXIMA;
+	return CAPACIDAD_MAXIMA;
 }
 
 
 int crea_sala(int capacidad){
-  CAPACIDAD_MAXIMA = capacidad;
-  asientos_libres_variable = CAPACIDAD_MAXIMA;
-  asientos=(int*)malloc(CAPACIDAD_MAXIMA*sizeof(int));
-  if (asientos == NULL){
-    exit(-1);
-  }
-  for (int count = 1; count <= CAPACIDAD_MAXIMA; count++){
-    *(asientos + count) = -1;
-  }
-  return 0;
+	CAPACIDAD_MAXIMA = capacidad;
+	asientos_libres_variable = CAPACIDAD_MAXIMA;
+	asientos=(int*)malloc(CAPACIDAD_MAXIMA*sizeof(int));
+	if (asientos == NULL){
+		exit(-1);
+	}
+	for (int count = 1; count <= CAPACIDAD_MAXIMA; count++){
+		*(asientos + count) = -1;
+	}
+	return 0;
 }
 
 
 int elimina_sala(){
-  if (asientos == NULL){
-    return -1;
-  }
-  free(asientos);
-  return 0;
+	if (asientos == NULL){
+		return -1;
+	}
+	free(asientos);
+	return 0;
 }
