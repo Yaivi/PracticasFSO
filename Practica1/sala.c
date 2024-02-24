548721aa316bd630e89cf3d2088cf3707117b61b
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-02-24 15:29:28 +0000

    PASA LOS TESTS DADOS POR EL PROFESORADO
    En este punto sala.c pasa los tests dados por los profesores.

--- Practica1/sala.c
+++ Practica1/sala.c
@@ -19,7 +19,9 @@ int reserva_asiento(int id_persona){
 	for (int count = 1; count<=CAPACIDAD_MAXIMA; count++){
 		if (*(asientos + count) == -1){
 			asientos_ocupados_variable++;
-			return *(asientos + count);
+			asientos_libres_variable--;
+			*(asientos + count) = id_persona;
+			return count;
 		}
 	}
 }
@@ -29,8 +31,11 @@ int libera_asiento(int id_asiento){
 	if (id_asiento < 0){
 		return -1;
 	}
+	int id_persona_anterior = *(asientos + id_asiento);
 	*(asientos + id_asiento) = -1;
-    asientos_ocupados_variable--;
+        asientos_ocupados_variable--;
+        asientos_libres_variable++;
+        return id_persona_anterior;
 }
 
 
@@ -64,12 +69,12 @@ int crea_sala(int capacidad){
 	asientos_libres_variable = CAPACIDAD_MAXIMA;
 	asientos=(int*)malloc(CAPACIDAD_MAXIMA*sizeof(int));
 	if (asientos == NULL){
-		exit(-1);
+		return -1;
 	}
 	for (int count = 1; count <= CAPACIDAD_MAXIMA; count++){
 		*(asientos + count) = -1;
 	}
-	return 0;
+	return CAPACIDAD_MAXIMA;
 }
 
 
