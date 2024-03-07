7d85db929bc7f737503f9c181b0a8e63a32de097
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-03-07 12:22:08 +0000

    Espacios añadidos.

--- Practica1/sala.h
+++ Practica1/sala.h
@@ -4,6 +4,7 @@
 //   -1 si no hay asientos libres
 int reserva_asiento (int id_persona);
 
+
 // Método para liberar un asiento reservado.
 // Devuelve:
 //   El identificador de la persona que ocupada el asiento
@@ -11,20 +12,25 @@ int reserva_asiento (int id_persona);
 //   -1 si ha habido algún error
 int libera_asiento (int id_asiento); 
 
+
 // Método que devuelve:
 //   el identificador de la persona que lo ocupa
 //   0 si no está ocupado
 //   -1 si ha habido un error a la hora de ejecutar el método
 int estado_asiento(int id_asiento);
 
+
 // Método que devuelve el número de asientos libres.
 int asientos_libres ();
+
 
 // Método que devuelve el número de asientos ocupados.
 int asientos_ocupados (); 
 
+
 // Método que devuelve la capacidad de la sala.
 int capacidad_sala ();
+
 
 // Método para crear y reservar espacio para una sala.
 // Se le pasa como argumento el número de asientos máximos.
@@ -32,6 +38,7 @@ int capacidad_sala ();
 //   La capacidad de la sala
 //   -1 si no se ha podido crear la sala.
 int crea_sala (int capacidad);
+
 
 // Método para eliminar una sala y liberar la memoria.
 // Devuelve:
