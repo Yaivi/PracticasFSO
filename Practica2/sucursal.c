5590c7cfe0548730b381258092e8dd54ee0345fb
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-03-21 18:02:40 +0000

    Añadidos algunos comentarios. Falta hacer aún el excecl().

--- Practica2/sucursal.c
+++ Practica2/sucursal.c
@@ -1,20 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <"unistd.h">
+#include <"string.h">
 #include "sala.h"
 
 void crea_sucursal (const char* ciudad, int capacidad);
 
 void crea_sucursal (const char* ciudad, int capacidad) {
-// Crear un proceso hijo que lance una terminal donde se
-// ejecute el mini-shell añadido a la práctica 1
-// Deberás emplear las llamadas al sistema fork() y exec()
+  // Crear un proceso hijo que lance una terminal donde se
+  // ejecute el mini-shell añadido a la práctica 1
+  // Deberás emplear las llamadas al sistema fork() y exec()
+  pid_t pid = fork();
+  
+  if (pid == -1){
+    exit(-1);
+  }
+  if (pid == 0){
+    // Proceso Hijo
+    //execl();
+    
+    //Si falla, hará exit(-1), este código no debería
+    //ejecutarse si todo va bien.
+    exit(-1);
+  }
+  
+  
+
 }
 
 
-
-
 int main() {
-    char nombresala[...];
+    char nombresala[100];
     int capacidad;
     while (1) {
         // Pedir nombre de nueva sala y capacidad
