ee94c01ffd636a9b8f22f04f4662c1a8a95b743f
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-03-21 16:40:47 +0000

    DESDE AQUI HAY QUE HACER EL SHELL QUE CREA FORK

---
+++ Practica2/sucursal.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sala.h"
+
+void crea_sucursal (const char* ciudad, int capacidad);
+
+void crea_sucursal (const char* ciudad, int capacidad) {
+// Crear un proceso hijo que lance una terminal donde se
+// ejecute el mini-shell añadido a la práctica 1
+// Deberás emplear las llamadas al sistema fork() y exec()
+}
+
+
+
+
+int main() {
+    char nombresala[...];
+    int capacidad;
+    while (1) {
+        // Pedir nombre de nueva sala y capacidad
+        if (!strcmp("salir",nombresala)){
+            break;
+        }
+        crea_sucursal (nombresala, capacidad);
+        } // termina el bucle principal
+}
