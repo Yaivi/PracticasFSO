9d85f20a0f13c8d53bdc199ad7b01ae5327fd1bc
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-04-03 20:08:43 +0100

    Se cierra al eliminar sala.

--- Practica2/sala.c
+++ Practica2/sala.c
@@ -169,6 +169,7 @@ int main(int argc, char *argv[]){
 	  		opcion_usuario = 0;	  	
 	  		printf("Ha seleccionado cerrar y eliminar sala.\n");
 	  		elimina_sala();
+			exit(0);
 	  		break;	  	
 	  	default:
 	  		printf("Opción inválida. Escriba un número válido.\n");
