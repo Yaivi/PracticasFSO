deab2cbd5ad3725c4bc12a22d92dbf44e1eb20ad
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-04-12 13:57:53 +0100

    Update sala.h

--- Practica3/sala.h
+++ Practica3/sala.h
@@ -56,8 +56,7 @@ int recupera_estado_sala(const	char*	ruta_fichero);
 
 
 //
-int	guarda_estadoparcial_sala(const char*	ruta_fichero,	size_t num_asientos,	int*	id_asientos);
-
+int guarda_estadoparcial_sala(const char*	ruta_fichero,	size_t num_asientos,	int*	id_asientos);
 
 //
 int	recupera_estadoparcial_sala(const	char*	ruta_fichero, size_t num_asientos, int*	id_asientos);
