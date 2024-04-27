45b7bb959c211453a241da43d4bf7ca79d7261eb
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-04-27 18:14:57 +0100

    Añadido anular asientos y mejorado estado

--- Practica3/misala.c
+++ Practica3/misala.c
@@ -40,15 +40,62 @@ int main(int argc, char *argv[]){
   if(strcmp(orden_opción, "crea") == 0){
     capacidad = atoi(argv[5]);
   }
-  
+
+	  
   else if (strcmp(orden_opción, "reserva") == 0 && strcmp(f,"-f") == 0){
     
   }  
-  
-  else if(strcmp(orden_opción, "anula") == 0 && strcmp(f,"-f") == 0){
+
+	  
+  else if(strcmp(orden_opción, "anula") == 0 && strcmp(f,"-f") == 0 && strcmp(argv[4], "-asientos") == 0){
+    int fd = open(ruta, O_RDONLY);
+    int contenido;
     
+    contenido = read(fd, &capacidad, sizeof(int));
+    if (contenido == -1) {
+      fprintf(stderr, "Error %d al leer el archivo: \n", errno);
+      exit(-1);
+    }
+    close(fd);
+    crea_sala(capacidad);
+    
+    int num_asientos_pasados = argc - 5;
+    int asientos_fuera_de_rango = 0;
+    int asientos_resultantes_a_anular = 0;
+    int *asientos_a_anular = (int*)malloc(num_asientos_pasados * sizeof(int));
+    if (asientos_a_anular == NULL){
+      return -1;
+    }
+    int *asientos_a_fuera_rango_vector = (int*)malloc(num_asientos_pasados * sizeof(int));
+    int index = 0;
+    
+    for (int i = 0; i < num_asientos_pasados; i++){
+      if (comprobar_valor_id_asiento(atoi(argv[5+i]), capacidad) == 0){
+        *(asientos_a_anular + index) = atoi(argv[5+i]);
+        index++;
+      } else {
+        printf("ATOI %d\n", atoi(argv[5+i]));
+        printf("LUGAR: %d\n", asientos_fuera_de_rango);
+        asientos_a_fuera_rango_vector[asientos_fuera_de_rango] = atoi(argv[5+i]);
+        asientos_fuera_de_rango++;
+      }
+    }
+    
+    asientos_resultantes_a_anular = num_asientos_pasados - asientos_fuera_de_rango;     
+    guarda_estadoparcial_sala(ruta, asientos_resultantes_a_anular, asientos_a_anular);
+
+    if (asientos_fuera_de_rango > 0){
+      fprintf(stderr, "No se han podido guardar estos asientos: ");
+      for (int i = 0; i < asientos_fuera_de_rango; i++){
+        printf("%d ", asientos_a_fuera_rango_vector[i]);
+      }
+      printf(" \n");
+    }
+    //Libera la memoria del vector auxiliar para los asientos a anular
+    free(asientos_a_anular);
   }
-  
+
+	  
   else if(strcmp(orden_opción, "estado") == 0 && strcmp(f,"-f") == 0){
     int fd = open(ruta, O_RDONLY);
     int contenido;
@@ -62,15 +109,18 @@ int main(int argc, char *argv[]){
     crea_sala(capacidad);
     
     recupera_estado_sala(ruta);
+    printf("Capacidad de la sala: %d\n", capacidad_sala());
+    printf("Asientos Ocupados: %d\n", asientos_ocupados());
+    printf("Asientos libres: %d\n", asientos_libres());
     for (int i=1; i<capacidad+1; i++){
       printf("Asiento %d %d \n", i, estado_asiento(i));
     }
   }
-  
+
+	  
   else{
     fprintf(stderr, "Orden no válida\n");
   }
-  
   return 0;
 }  
   
