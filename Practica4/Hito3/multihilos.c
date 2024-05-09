9dbc0254a994191638c115b8415e119b2235c68a
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-05-09 17:53:32 +0100

    Add files via upload

---
+++ Practica4/Hito3/multihilos.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "sala.h"
+
+int main(int argc, char *argv[]){
+  int n = atoi(argv[1]);
+  
+}
