7169e0c3375b0e2396bb72a415c5e52f5e66cbba
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-05-17 14:06:46 +0100

    misala.c de Prac3 Corregida

--- Practica4/Hito1/misala.c
+++ Practica4/Hito1/misala.c
@@ -7,11 +7,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <pthread.h>
 #include "sala.h"
 
-
-#define MAX_HILOS 10
 
 int comprobar_valor_id_asiento(int opcion_usuario, int capacidad){
   if (opcion_usuario <= 0 || opcion_usuario > capacidad){
@@ -20,22 +17,32 @@ int comprobar_valor_id_asiento(int opcion_usuario, int capacidad){
     return 0;
 }
 
-int comprobar_error_en_misala(){
-	if (errno != 0){
-		fprintf(stderr, "Se ha producido un error con código %d: %s", errno, strerror(errno));
-		fflush(stderr);
-		errno = 0;
-		return -1;
-	}
-	return 0;
+int comprobar_valor_id_persona(int opcion_usuario, int capacidad){
+  if (opcion_usuario <= 0){
+    return -1;
+  }
+    return 0;
 }
 
+int comprobar_error_en_misala(){
+  if (errno != 0){
+    fprintf(stderr, "Se ha producido un error con código %d: %s", errno, strerror(errno));
+    fflush(stderr);
+    errno = 0;
+    return -1;
+    }
+    return 0;
+}
+
+int comprobar_capacidad_en_misala(){
+  fprintf(stderr, "Se ha pasado como parámetro una capacidad negativa o 0\n");
+  return -1;
+}
 
 
 int main(int argc, char *argv[]){
   int capacidad;
   int fd;
-  //crea_sala(capacidad); // Crea una capacidad con el valor de argv[2]
   
   char* orden_opción = argv[1];
   char* f = argv[2];
@@ -44,7 +51,9 @@ int main(int argc, char *argv[]){
   if(strcmp(orden_opción, "crea") == 0 && strcmp(f,"-f") == 0){
     if ((fd = open(ruta, O_RDONLY)) == -1) {
       capacidad = atoi(argv[5]);
-      crea_sala(capacidad);
+      if (crea_sala(capacidad) == -1){
+        return -1;
+      }
       guarda_estado_sala(ruta);
       elimina_sala();
       printf("Se ha creado el archivo con los datos pasados\n");
@@ -58,76 +67,145 @@ int main(int argc, char *argv[]){
   
   else if(strcmp(orden_opción, "crea") == 0 && strcmp(f,"-o") == 0){
     capacidad = atoi(argv[5]);
-    crea_sala(capacidad);
+    if (crea_sala(capacidad) == -1){
+      comprobar_capacidad_en_misala();
+      return -1;
+    }
     guarda_estado_sala(ruta);
     elimina_sala();
   }
 
-	  
-  else if (strcmp(orden_opción, "reserva") == 0 && strcmp(f,"-f") == 0){
+else if (strcmp(orden_opción, "reserva") == 0 && strcmp(f, "-f") == 0) {
+    int contador_valor_negativo = 0;
     int fd = open(ruta, O_RDWR);
-    int contenido;
     if (fd == -1) {
         comprobar_error_en_misala();
         return -1;    
     }
-    contenido = read(fd, &capacidad, sizeof(int));
-    if (contenido == -1) {
+    
+    int capacidad;
+    if (read(fd, &capacidad, sizeof(int)) == -1) {
         comprobar_error_en_misala();
+        close(fd);
         return -1;  
     }
-    crea_sala(capacidad);
-    recupera_estado_sala(ruta);
-    if (argc-3 > asientos_libres()) {
-        fprintf(stderr, "No hay suficientes asientos libres, faltan %d asientos", argc-3-asientos_libres());
-        fflush(stderr);
-        errno = 0;
+    close(fd);
+    
+    if (crea_sala(capacidad) == -1) {
+        comprobar_capacidad_en_misala();
         return -1;
     }
-    for (int i = 4; i<argc; i++) {
-        reserva_asiento(atoi(argv[i]));
-    }
-    guarda_estado_sala(ruta);
-    elimina_sala();
-    printf("Reserva completa\n");
-  }   
-
-	  
-  else if(strcmp(orden_opción, "anula") == 0 && strcmp(f,"-f") == 0 && strcmp(argv[4], "-asientos") == 0){
-    int fd = open(ruta, O_RDONLY);
-    int contenido;
-    
-    contenido = read(fd, &capacidad, sizeof(int));
-    if (contenido == -1) {
-      fprintf(stderr, "Error %d al leer el archivo: \n", errno);
-      exit(-1);
-    }
-    
-    crea_sala(capacidad);
     recupera_estado_sala(ruta);
     
-    int num_asientos_pasados = argc;
-      
-    for (int i = 5; i < num_asientos_pasados; i++){
-          int asiento = atoi(argv[i]);
-          libera_asiento(asiento);
+    int num_asientos = argc - 4;
+    int num_asientos_validos = 0;
+    int *lista_asientos = malloc(num_asientos * sizeof(int));
+    if (lista_asientos == NULL) {
+        comprobar_error_en_misala();
+        return -1;
     }
-
-    guarda_estado_sala(ruta);
-    elimina_sala();
     
-    return 0;
-  }
+    for (int i = 4; i < argc; i++) {
+        int id_persona = atoi(argv[i]);
+        if (comprobar_valor_id_persona(id_persona, capacidad) == -1) {
+            contador_valor_negativo++;
+        } else {
+            lista_asientos[num_asientos_validos++] = reserva_asiento(id_persona);
+        }
+    }
+    
+    if (num_asientos_validos > asientos_libres()) {
+        fprintf(stderr, "No hay suficientes asientos libres, faltan %d asientos\n", num_asientos_validos - asientos_libres());
+        fflush(stderr);
+        errno = 0;
+        free(lista_asientos);
+        return -1;
+    }
+    
+    guarda_estadoparcial_sala(ruta, num_asientos_validos * sizeof(int), lista_asientos);
+    elimina_sala();
+    free(lista_asientos);
+    
+    if (contador_valor_negativo > 0) {
+        printf("Reserva completa pero con fallos\n");
+    } else if (contador_valor_negativo == num_asientos) {
+        printf("Reserva no completada\n");
+    } else {
+        printf("Reserva completada\n");
+    }
+}
 
-	  
+  
+  else if (strcmp(orden_opción, "anula") == 0 && strcmp(f, "-f") == 0 && strcmp(argv[4], "-asientos") == 0) {
+    int fd = open(ruta, O_RDWR);
+    if (fd == -1) {
+        comprobar_error_en_misala();
+        return -1;
+    }
+    
+    int capacidad;
+    if (read(fd, &capacidad, sizeof(int)) == -1) {
+        comprobar_error_en_misala();
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    
+    if (crea_sala(capacidad) == -1) {
+        comprobar_capacidad_en_misala();
+        return -1;
+    }    
+    recupera_estado_sala(ruta);
+    
+    int num_asientos = argc - 5;
+    int contador_valor_negativo = 0;
+    int *lista_asientos = malloc(num_asientos * sizeof(int));
+    if (lista_asientos == NULL) {
+        comprobar_error_en_misala();
+        return -1;
+    }
+    
+    int num_asientos_validos = 0;
+    for (int i = 5; i < argc; i++) {
+        int num_asiento = atoi(argv[i]);
+        if (num_asiento < 1 || num_asiento > capacidad) {
+            contador_valor_negativo++;
+        } else {
+            lista_asientos[num_asientos_validos++] = num_asiento;
+        }
+    }
+    
+    for (int i = 0; i < num_asientos_validos; i++) {
+        libera_asiento(lista_asientos[i]);
+    }
+    
+    if (guarda_estadoparcial_sala(ruta, num_asientos_validos * sizeof(int), lista_asientos) == -1) {
+        comprobar_error_en_misala();
+        free(lista_asientos);
+        return -1;
+    }
+    
+    elimina_sala();
+    free(lista_asientos);
+    
+    if (contador_valor_negativo > 0 && contador_valor_negativo < num_asientos) {
+        printf("Anulación completa pero con fallos\n");
+    } else if (contador_valor_negativo == num_asientos) {
+        printf("Anulación no completada\n");
+    } else {
+        printf("Anulación completada\n");
+    }
+}
+
+
   else if(strcmp(orden_opción, "estado") == 0 && strcmp(f,"-f") == 0){
     int fd = open(ruta, O_RDONLY);
     int contenido;
     
     contenido = read(fd, &capacidad, sizeof(int));
     if (contenido == -1) {
-      fprintf(stderr, "Error %d al leer el archivo: \n", errno);
-      exit(-1);
+        comprobar_error_en_misala();
+        return -1;  
     }
     close(fd);
     crea_sala(capacidad);
@@ -140,12 +218,10 @@ int main(int argc, char *argv[]){
       printf("Asiento %d %d \n", i, estado_asiento(i));
     }
   }
-  
+
 	  
   else{
     fprintf(stderr, "Orden no válida\n");
   }
   return 0;
 }
-
-
