a045520011f40e2d8c72899a74c57a96c1620ff2
PedroPDiaz <145754989+PedroPDiaz@users.noreply.github.com>
2024-05-20 22:01:49 +0100

    Sigue quedando corrupta la sala.

--- Practica4/Hito2/sala.c
+++ Practica4/Hito2/sala.c
@@ -4,8 +4,8 @@
 #include <string.h>
 #include "sala.h"
 #include <fcntl.h>
-#include <string.h>
 #include <pthread.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -25,8 +25,9 @@ int comprueba_id_persona(int id_persona){
 	return 0;
 }
 
+
 int comprueba_sala(){
-	if (asientos_libres_variable < 0){
+	if (asientos_libres < 0){
 		return -1;
 	}
 	return 0;
@@ -41,6 +42,7 @@ int comprueba_id_asiento(int id_asiento){
 	}
 	return 0;
 }
+
 
 int comprobar_error(){
 	if (errno != 0){
@@ -73,133 +75,136 @@ int comprobar_capacidad_salafichero_misma_salaactual(const char* ruta_fichero){
     return 0;
 }
 
+
 int reserva_asiento(int id_persona){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1){
-        pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-    
-    if (comprueba_id_persona(id_persona) == -1){
-        pthread_mutex_unlock(&lock);
-        return -1;
-    }
-    
-    if (asientos_libres_variable == 0){
-        pthread_mutex_unlock(&lock);
-        return -1;
-    }
-    
-    for (int count = 1; count <= CAPACIDAD_MAXIMA; count++){
-        if (*(asientos + count - 1) == -1){
-            asientos_ocupados_variable++;
-            asientos_libres_variable--;
-            *(asientos + count - 1) = id_persona;
-            pthread_mutex_unlock(&lock);
-            return count;
+        pthread_mutex_lock(&lock);
+        if (comprueba_sala() == -1){
+                pthread_mutex_unlock(&lock);
+                return -1; 
         }
-    }
-    pthread_mutex_unlock(&lock);
-    return -1;
+	// Si el id_persona es menor que 0 dará error.
+	if (comprueba_id_persona(id_persona) == -1){
+		pthread_mutex_unlock(&lock);
+		return -1;
+	}
+	// Si no hay ningún asiento libre dará error.
+	if (asientos_libres_variable == 0){
+	        pthread_mutex_unlock(&lock);
+		return -1;
+	}
+	for (int count = 1; count<=CAPACIDAD_MAXIMA; count++){
+		if (*(asientos + count -1) == -1){
+			asientos_ocupados_variable++;
+			asientos_libres_variable--;
+			*(asientos + count -1) = id_persona;
+			pthread_mutex_unlock(&lock);
+			return count;
+		}
+	}
 }
+
 
 int libera_asiento(int id_asiento){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1 || comprueba_id_asiento(id_asiento) == -1){
+        pthread_mutex_lock(&lock);
+        if (comprueba_sala() == -1){
+                pthread_mutex_unlock(&lock);
+                return -1; 
+        }
+        if (comprueba_id_asiento(id_asiento) == -1){
+                pthread_mutex_unlock(&lock);
+                return -1;
+        }
+	int id_persona_anterior = *(asientos + id_asiento -1);
+	*(asientos + id_asiento -1) = -1;
+        asientos_ocupados_variable--;
+        asientos_libres_variable++;
         pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-
-	int id_persona_anterior = *(asientos + id_asiento - 1);
-	*(asientos + id_asiento - 1) = -1;
-    asientos_ocupados_variable--;
-    asientos_libres_variable++;
-    pthread_mutex_unlock(&lock);
-    return id_persona_anterior;
+        return id_persona_anterior;
 }
+
 
 int estado_asiento(int id_asiento){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1 || comprueba_id_asiento(id_asiento) == -1){
-        pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-
-    int estado = *(asientos + id_asiento - 1);
-    pthread_mutex_unlock(&lock);
-    return estado == -1 ? 0 : estado;
+        pthread_mutex_lock(&lock);
+        if (comprueba_sala() == -1){
+                pthread_mutex_unlock(&lock);
+                return -1; 
+        }
+        if (comprueba_id_asiento(id_asiento)){
+                pthread_mutex_unlock(&lock);
+                return -1;
+        }
+	if (*(asientos + id_asiento -1) != -1){
+	        pthread_mutex_unlock(&lock);
+		return *(asientos + id_asiento -1);
+	}
+	else{
+	        pthread_mutex_unlock(&lock);
+		return 0;
+	}
 }
+
 
 int asientos_libres(){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1){
+        pthread_mutex_lock(&lock);
+        if (comprueba_sala() == -1){
+                return -1; 
+        }
         pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-    int libres = asientos_libres_variable;
-    pthread_mutex_unlock(&lock);
-    return libres;
+	return asientos_libres_variable;
 }
+
 
 int asientos_ocupados(){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1){
-        pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-    int ocupados = asientos_ocupados_variable;
-    pthread_mutex_unlock(&lock);
-    return ocupados;
-}
+        if (comprueba_sala() == -1){
+                return -1; 
+        }
+        return asientos_ocupados_variable;
+} 
+
 
 int capacidad_sala(){
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1){
+        pthread_mutex_lock(&lock);
+        if (comprueba_sala() == -1){
+                return -1; 
+        }
         pthread_mutex_unlock(&lock);
-        return -1; 
-    }
-    int capacidad = CAPACIDAD_MAXIMA;
-    pthread_mutex_unlock(&lock);
-    return capacidad;
+	return CAPACIDAD_MAXIMA;
 }
 
+
 int crea_sala(int capacidad){
-    pthread_mutex_lock(&lock);
 	CAPACIDAD_MAXIMA = capacidad;
 	asientos_libres_variable = CAPACIDAD_MAXIMA;
 	asientos_ocupados_variable = 0;
 
-    if (CAPACIDAD_MAXIMA <= 0){
-        pthread_mutex_unlock(&lock);
-	    return -1;
+  if (CAPACIDAD_MAXIMA <= 0){
+	  return -1;
 	}
         
-    asientos = (int*)malloc(CAPACIDAD_MAXIMA * sizeof(int));
+    asientos=(int*)malloc(CAPACIDAD_MAXIMA*sizeof(int));
+  
     if (asientos == NULL){
-        pthread_mutex_unlock(&lock);
-        return -1;
+      return -1;
     }
 
     for (int count = 1; count <= CAPACIDAD_MAXIMA; count++){
-        *(asientos + count - 1) = -1;
+      *(asientos + count - 1) = -1;
     }
-    pthread_mutex_unlock(&lock);
     return CAPACIDAD_MAXIMA;
 }
 
+
 int elimina_sala(){
-    pthread_mutex_lock(&lock);
-    if (asientos == NULL || CAPACIDAD_MAXIMA <= 0){
-        pthread_mutex_unlock(&lock);
-        return -1;
+  if (asientos == NULL || CAPACIDAD_MAXIMA <= 0){
+      return -1;
     }
     free(asientos);
     CAPACIDAD_MAXIMA = -1;
     asientos_libres_variable = -1;
     asientos_ocupados_variable = -1;
-    pthread_mutex_unlock(&lock);
     return 0;
 }
+
 
 int coger_tamaño_bloque(const char* ruta_fichero) {
     struct stat estado;
@@ -210,114 +215,291 @@ int coger_tamaño_bloque(const char* ruta_fichero) {
     return estado.st_blksize;
 }
 
+
 int guarda_estado_sala(const char* ruta_fichero){
-    ssize_t bytes_escritos;
-    pthread_mutex_lock(&lock);
-    if (comprueba_sala() == -1){
-        printf("Error: no se ha creado una sala");
-        pthread_mutex_unlock(&lock);
-        return -1;
+    if (comprobar_capacidad_salafichero_misma_salaactual(ruta_fichero) == -1){
+      return -1;
     }
-    
+
+    ssize_t bytes_escritos;
+    if (comprueba_sala() == -1){
+      printf("Error: no se ha creado una sala");
+      return -1;
+    }
     int fd = open(ruta_fichero, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+
     if (fd == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }
     int tam_bloque  = coger_tamaño_bloque(ruta_fichero);
     
-    bytes_escritos = write(fd, &CAPACIDAD_MAXIMA, sizeof(int));
+    // Guardar en el fichero CAPACIDAD_MAXIMA
+    write(fd, &CAPACIDAD_MAXIMA, sizeof(int));
     if (bytes_escritos == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }
     
-    bytes_escritos = write(fd, &asientos_libres_variable,  sizeof(int));
+    // Guardar en el fichero asientos_ocupados_variable
+    write(fd, &asientos_libres_variable,  sizeof(int));
     if (bytes_escritos == -1) {
+        printf("es culpa de asientos libres");
         comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }    
 
-    bytes_escritos = write(fd, &asientos_ocupados_variable, sizeof(int));
+    // Guardar en el fichero asientos_libres_variable
+    write(fd, &asientos_ocupados_variable, sizeof(int));
     if (bytes_escritos == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }
 
+    // Guardar en el fichero el vector asientos
     int num_bloques = CAPACIDAD_MAXIMA / tam_bloque;
     int resto = CAPACIDAD_MAXIMA % tam_bloque;
     for (int i = 0; i < num_bloques; i++) {
-        bytes_escritos = write(fd, &asientos[i * tam_bloque], tam_bloque * sizeof(int));
-        if (bytes_escritos == -1) {
+      bytes_escritos = write(fd, &asientos[i*tam_bloque], tam_bloque*sizeof(int));
+      if (bytes_escritos == -1) {
+          comprobar_error();
+          return -1;
+      }
+    }
+    // Si hay bloques que no estén llenos
+    if (resto > 0){
+      bytes_escritos = write(fd, &asientos[num_bloques * tam_bloque], resto*sizeof(int));
+      if (bytes_escritos == -1) {
+      comprobar_error();
+      return -1;
+      }
+    }
+      
+    close(fd);
+    return 0;
+}
+
+
+int recupera_estado_sala(const char* ruta_fichero){
+    int nueva_CAPACIDAD_MAXIMA;
+    int fd = open(ruta_fichero, O_RDONLY);
+    if (fd == -1) {
+        comprobar_error();
+        return -1;
+    }
+    int contenido;
+    contenido = read(fd, &nueva_CAPACIDAD_MAXIMA, sizeof(int));
+    if (contenido == -1) {
+        comprobar_error();
+        return -1;
+    }
+    if (nueva_CAPACIDAD_MAXIMA != CAPACIDAD_MAXIMA){
+        comprobar_error();
+        return -1;
+    }
+    contenido = read(fd, &asientos_libres_variable, sizeof(int));
+    if (contenido == -1) {
+        comprobar_error();
+        return -1;
+    }
+    contenido = read(fd, &asientos_ocupados_variable, sizeof(int));
+    if (contenido == -1) {
+        comprobar_error();
+        return -1;
+    }
+
+    int tam_bloque  = coger_tamaño_bloque(ruta_fichero);
+    int num_bloques = CAPACIDAD_MAXIMA / tam_bloque;
+    int resto = CAPACIDAD_MAXIMA % tam_bloque;
+    for (int i = 0; i < num_bloques; i++) {
+        contenido = read(fd, &asientos[i*tam_bloque], tam_bloque*sizeof(int));
+        if (contenido == -1) {
             comprobar_error();
-            pthread_mutex_unlock(&lock);
             return -1;
         }
     }
     if (resto > 0){
-        bytes_escritos = write(fd, &asientos[num_bloques * tam_bloque], resto * sizeof(int));
-        if (bytes_escritos == -1) {
+        contenido = read(fd, &asientos[num_bloques * tam_bloque], resto*sizeof(int));      
+        if (contenido == -1) {
+          comprobar_error();
+          return -1;
+        }
+    }    
+    close(fd);
+    
+    // Ajuste de las variables de asientos libres y ocupados
+    asientos_ocupados_variable = 0;
+    asientos_libres_variable = 0;
+    for (int i = 1; i <= CAPACIDAD_MAXIMA; i++){
+      if (*(asientos + i -1) == -1){
+        asientos_libres_variable++;
+      }
+    }
+    asientos_ocupados_variable = CAPACIDAD_MAXIMA - asientos_libres_variable;
+    
+    return 0;
+}
+
+int guarda_estadoparcial_sala(const char* ruta_fichero, size_t num_asientos, int* id_asientos){
+    if (comprobar_capacidad_salafichero_misma_salaactual(ruta_fichero) == -1){
+      return -1;
+    }
+    
+    for (size_t i = 0; i < num_asientos/sizeof(int); i++) {
+        int asiento = id_asientos[i];
+        printf("Asiento %d\n", asiento);
+        if (asiento < 1 || asiento > CAPACIDAD_MAXIMA) {
+            fprintf(stderr, "Hay un asiento o más que no se encuentran en el rango de la sala.\n");
             comprobar_error();
-            pthread_mutex_unlock(&lock);
             return -1;
         }
     }
+    
+    int fd = open(ruta_fichero, O_WRONLY);
+    if (fd == -1) {
+        comprobar_error();
+        return -1;
+    }
+    
+    off_t offset = 3 * sizeof(int);
+    for (size_t i = 0; i < num_asientos/sizeof(int); i++){
+        int id_asiento_a_guardar = id_asientos[i]-1;
+        if (comprueba_id_asiento(id_asiento_a_guardar) == -1){
+            return -1;
+        }
+
+        off_t nuevo_offset = offset + id_asiento_a_guardar*sizeof(int);
+        lseek(fd, nuevo_offset, SEEK_SET);
+        int id_persona_a_guardar = asientos[id_asiento_a_guardar];
+        if (write(fd, &id_persona_a_guardar, sizeof(int)) == -1){
+            comprobar_error();
+            return -1;
+        } 
+    }
     close(fd);
-    pthread_mutex_unlock(&lock);
+    
+    // Ajuste de las variables de asientos libres y ocupados
+    asientos_ocupados_variable = 0;
+    asientos_libres_variable = 0;
+    for (int i = 1; i <= CAPACIDAD_MAXIMA; i++){
+        if (*(asientos + i -1) == -1){
+            asientos_libres_variable++;
+        }
+    }
+    asientos_ocupados_variable = CAPACIDAD_MAXIMA - asientos_libres_variable;
+    
     return 0;
 }
 
-int recupera_estado_sala(const char* ruta_fichero){
-    pthread_mutex_lock(&lock);
+
+int recupera_estadoparcial_sala(const char* ruta_fichero, size_t num_asientos, int* id_asientos){
     if (comprobar_capacidad_salafichero_misma_salaactual(ruta_fichero) == -1){
-        pthread_mutex_unlock(&lock);
-        return -1;
+      return -1;
     }
 
-    ssize_t bytes_leidos;
+    for (size_t i = 0; i < num_asientos/sizeof(int); i++) {
+      int asiento = id_asientos[i];
+      printf("Asiento %d\n", asiento);
+      if (asiento < 1 || asiento > CAPACIDAD_MAXIMA) {
+        fprintf(stderr, "Hay un asiento o más que no se encuentran en el rango de la sala.\n");
+        comprobar_error();
+        return -1;
+      }
+  }
+    
     int fd = open(ruta_fichero, O_RDONLY);
     if (fd == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
+        return -1;
+    }
+    int contenido;   
+    int asiento_a_leer;
+    off_t offset = 3 * sizeof(int);
+    
+    for (size_t i = 0; i < num_asientos; i++){
+      int id_asiento_a_recuperar = id_asientos[i]-1;
+      if (comprueba_id_asiento(id_asiento_a_recuperar) == -1){
+        return -1;
+      }
+
+      off_t nuevo_offset = offset + id_asiento_a_recuperar*sizeof(int);
+      lseek(fd, nuevo_offset, SEEK_SET);
+      contenido = read(fd, &asientos[id_asientos[i]-1], sizeof(int));
+      if (contenido == -1){
+        comprobar_error();
+        return -1;
+        } 
+    }
+    close(fd);
+    
+    
+    int asientos_libres_fichero = 0;
+    int asientos_ocupados_fichero = 0;
+    fd = open(ruta_fichero, O_RDONLY);
+    if (fd == -1) {
+        comprobar_error();
         return -1;
     }
 
-    bytes_leidos = read(fd, &CAPACIDAD_MAXIMA, sizeof(int));
-    if (bytes_leidos == -1) {
+    lseek(fd, sizeof(int), SEEK_SET);
+    contenido = read(fd, &asientos_libres_fichero, sizeof(int));
+    if (contenido == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }
-    bytes_leidos = read(fd, &asientos_libres_variable,  sizeof(int));
-    if (bytes_leidos == -1) {
+    contenido = read(fd, &asientos_ocupados_fichero, sizeof(int));
+    if (contenido == -1) {
         comprobar_error();
-        pthread_mutex_unlock(&lock);
-        return -1;
-    }
-    bytes_leidos = read(fd, &asientos_ocupados_variable, sizeof(int));
-    if (bytes_leidos == -1) {
-        comprobar_error();
-        pthread_mutex_unlock(&lock);
-        return -1;
-    }
-
-    asientos = (int*)malloc(CAPACIDAD_MAXIMA * sizeof(int));
-    if (asientos == NULL){
-        comprobar_error();
-        pthread_mutex_unlock(&lock);
-        return -1;
-    }
-    bytes_leidos = read(fd, asientos, CAPACIDAD_MAXIMA * sizeof(int));
-    if (bytes_leidos == -1) {
-        comprobar_error();
-        pthread_mutex_unlock(&lock);
         return -1;
     }
     close(fd);
-    pthread_mutex_unlock(&lock);
+  
+    if (asientos_libres() != asientos_libres_fichero || asientos_ocupados() != asientos_ocupados_fichero){
+      // Ajuste de las variables de asientos libres y ocupados
+      asientos_ocupados_variable = 0;
+      asientos_libres_variable = 0;
+      for (int i = 1; i <= CAPACIDAD_MAXIMA; i++){
+        if (*(asientos + i -1) == -1){
+          asientos_libres_variable++;
+        }
+      }
+      asientos_ocupados_variable = CAPACIDAD_MAXIMA - asientos_libres_variable;
+    }
     return 0;
 }
+
+/**
+int main() {
+  printf("---------------------------------\n");
+  crea_sala(10);
+  guarda_estado_sala("Documentos/Practica3/pruebaGP.txt");
+  for (int i = 1; i < 9; i++){
+     reserva_asiento(i*101);
+  }  
+  for (int i = 1; i < CAPACIDAD_MAXIMA+1; i++){
+    printf("ASIENTO : %d estado: %d\n", i, estado_asiento(i));
+  }    
+  int ids_asientos[4] = {1, 4, 6, 5}; // Ejemplo de IDs de asientos
+  guarda_estadoparcial_sala("Documentos/Practica3/pruebaGP.txt", sizeof(ids_asientos), ids_asientos);
+  elimina_sala();
+  printf("---------------------------------\n");
+  crea_sala(10);
+  //recupera_estadoparcial_sala("Documentos/Practica3/pruebaGP.txt", sizeof(ids_asientos), ids_asientos);
+  recupera_estado_sala("Documentos/Practica3/pruebaGP.txt");
+  reserva_asiento(222);
+  reserva_asiento(333);
+  reserva_asiento(444);
+  reserva_asiento(555);
+  reserva_asiento(666);
+  printf("ASIENTOS LIBRES %d\n", asientos_libres());
+  printf("ASIENTOS OCUPADOS %d\n", asientos_ocupados());
+  for (int i = 1; i < CAPACIDAD_MAXIMA+1; i++){
+    printf("ASIENTO : %d estado: %d\n", i, estado_asiento(i));
+  }    
+  elimina_sala();
+  
+  printf("TERMINADO\n");
+  return 0;
+}
+
+**/ 
